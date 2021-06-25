bcc360f6b57e746175da4e0165d7f385e6a1f96a
suxinwei <suxinwei@oppo.com>
2021-06-26 03:17:18 +0800

    feat: Scope字段增加提示，使ui效果更好

--- src/com/leroymerlin/commit/CommitPanel.java
+++ src/com/leroymerlin/commit/CommitPanel.java
@@ -2,21 +2,21 @@ package com.leroymerlin.commit;
 
 import com.intellij.openapi.project.Project;
 
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.File;
 import java.util.Enumeration;
 
+import javax.swing.*;
+
 /**
  * @author Damien Arrachequesne
  */
 public class CommitPanel implements ItemListener {
+    private static final String CHANGE_SCOPE_HINT = "---Please select the item below---";
+
     private JPanel mainPanel;
     private JComboBox<String> changeScope;
     private JTextField shortDescription;
@@ -37,7 +37,7 @@ public class CommitPanel implements ItemListener {
         File workingDirectory = new File(project.getBasePath());
         GitLogQuery.Result result = new GitLogQuery(workingDirectory).execute();
         if (result.isSuccess()) {
-            changeScope.addItem(""); // no value by default
+            changeScope.addItem(CHANGE_SCOPE_HINT);
             result.getScopes().forEach(changeScope::addItem);
         }
 
@@ -53,6 +53,16 @@ public class CommitPanel implements ItemListener {
         perfRadioButton.addItemListener(this);
         buildRadioButton.addItemListener(this);
         revertRadioButton.addItemListener(this);
+
+        changeScope.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String changeScopeStr = (String) changeScope.getSelectedItem();
+                if (CHANGE_SCOPE_HINT.equals(changeScopeStr)) {
+                    changeScope.setSelectedItem("");
+                }
+            }
+        });
 
         showFixView(fixRadioButton.isSelected());
     }
